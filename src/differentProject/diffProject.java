2a5a5b35c95d8735ebb3d52ed93500285fd97b7b
user <user@HOME>
2023-05-16 14:52:09 +0530

    Try to add in string Java repo

---
+++ src/differentProject/diffProject.java
@@ -0,0 +1,10 @@
+package differentProject;
+
+public class diffProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("DIfferent project in string java repo");
+	}
+
+}
