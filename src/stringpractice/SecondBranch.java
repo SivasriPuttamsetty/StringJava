d1af910f4ecf6d0bea44022f25d23e9324e86db4
user <user@HOME>
2023-05-16 14:18:20 +0530

    Second Project-Not a orphan branch

---
+++ src/stringpractice/SecondBranch.java
@@ -0,0 +1,10 @@
+package stringpractice;
+
+public class SecondBranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Second Project-Not a orphan branch");
+	}
+
+}
